646dab12c484a5f25b7da1488297c4f773e49a27
Nehal Ahmed <nehal.ahmed@tallysolutions.com>
2025-06-05 13:02:14 +0530

    Add newfile.js

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 // admin/src/api/products.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://e-backend-rf04.onrender.com/api/products' });
+const API = axios.create({ baseURL: 'https://e-backend-rf04.onrender.com/api' });
 
 export const addProduct    = data => API.post('/products', data);
 export const fetchProducts = ()   => API.get('/products');
